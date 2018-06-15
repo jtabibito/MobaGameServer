8a0be952ef15f5446b05b7d275fa2afd0e7bb537
shakker2c <jiln@vip.qq.com>
2018-06-15 15:25:23 +0800

    [shakker2c]异步mysql数据库模块

--- apps/test/main.cc
+++ apps/test/main.cc
@@ -9,8 +9,40 @@ using namespace std;
 #include "../../netbus/proto_man.h"
 #include "../../netbus/netbus.h"
 #include "proto/pf_cmd_map.h"
+#include "../../db/mysql_wrapper.h";
+
+static void
+on_query_cb(const char* err, std::vector<std::vector<std::string>>* result) {
+	if (err) {
+		printf("err %s\n", err);
+		return;
+	}
+
+	printf("success\n");
+}
+
+static void
+on_open_db(const char* err, void* context) {
+	if (err != NULL) {
+		printf("%s\n", err);
+		return;
+	}
+	printf("connect success\n");
+
+	mysql_wrapper::query(context, "insert into studentinfo (name, id, sex) values(\"¼ÆÁØ\", 1615925449, \"ÄÐ\")", on_query_cb);
+	//mysql_wrapper::query(context, "update studentinfo set name = \"shakker2c\" where id = 1615925449", on_query_cb);
+	// mysql_wrapper::close(context);
+}
+
+static void
+test_db() {
+	mysql_wrapper::connect("127.0.0.1", 3306, "class_data", "root", "null980311", on_open_db);
+}
 
 int main(int argc, char* argv[]) {
+
+	test_db();
+
 	proto_man::init(PROTO_BUF);
 	init_pf_cmd_map();
 
